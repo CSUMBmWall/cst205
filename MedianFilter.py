93cdf88dd0fafd4c57515d6a87f3139d3f3e0c5d
Matt <mwall@csumb.edu>
2016-02-02 19:56:58 -0800

    this shit is pissing me off

--- MedianFilter.py
+++ MedianFilter.py
@@ -1,8 +1,30 @@
 def medianFilter():   
-    redPixelList = [9]
-    greenPixelList = [9]
-    bluePixelList = [9]   
+    redPixelList = []
+    greenPixelList = []
+    bluePixelList = []   
     listMidValue = 5
-    finalPicLocation = "C:\cst\CST205\Project1Images\pic10.png"
+    finalPicLocation = "C:\cst205\Project1Images\picFinal.png"
     finalPic = makePicture(finalPicLocation)
     for pixelLength in range (0, getHeight(finalPic)):
+      for pixelWidth in range(0, getWidth(finalPic)):
+        for num in range (1, 10):
+        file = "C:\cst205\Project1Images\pic" + str(num) + ".png"
+          picture = makePicture(file)
+          pixel = getPixel(picture, pixelWidth, pixelLength)
+          redPixelList.append(getRed(pixel))
+          #greenPixelList.append(getGreen(pixel))
+          #bluePixelList.append(getBlue(pixel))
+      redPixelList.sort()
+      setRed(finalPic, redPixelList[listMidValue])
+      redPixelList[:] = []
+      del redPixelList[:]
+      
+      #greenPixelList.sort()
+      #setGreen(finalPic, bluePixelList[listMidValue])
+      #greenPixelList[:] = []
+      
+      #bluePixelList.sort()
+      #setBlue(finalPic, bluePixelList[listMidValue])
+      #bluePixelList[:] = []
+      return
+     
