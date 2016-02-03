63cec7d936d77a6345be2257d93a25d990ceb498
Matt <mwall@csumb.edu>
2016-02-02 20:49:10 -0800

    it works, but it will take a year to complete

--- MedianFilter.py
+++ MedianFilter.py
@@ -1,30 +1,30 @@
-def medianFilter():   
+def mediaFilter():   
     redPixelList = []
     greenPixelList = []
     bluePixelList = []   
-    listMidValue = 5
+    listMidValue = 1
     finalPicLocation = "C:\cst205\Project1Images\picFinal.png"
     finalPic = makePicture(finalPicLocation)
+    finalPicPixel = getPixel(finalPic, 0,0)
     for pixelLength in range (0, getHeight(finalPic)):
       for pixelWidth in range(0, getWidth(finalPic)):
-        for num in range (1, 10):
-        file = "C:\cst205\Project1Images\pic" + str(num) + ".png"
+        for num in range (1, 4):
+          file = "C:\cst205\Project1Images\pic" + str(num) + ".png"
           picture = makePicture(file)
           pixel = getPixel(picture, pixelWidth, pixelLength)
+          finalPicPixel = getPixel(finalPic, pixelWidth, pixelLength)
           redPixelList.append(getRed(pixel))
-          #greenPixelList.append(getGreen(pixel))
-          #bluePixelList.append(getBlue(pixel))
-      redPixelList.sort()
-      setRed(finalPic, redPixelList[listMidValue])
-      redPixelList[:] = []
-      del redPixelList[:]
+          greenPixelList.append(getGreen(pixel))
+          bluePixelList.append(getBlue(pixel))
+        redPixelList.sort()
+        setRed(finalPicPixel, redPixelList[listMidValue])
+        redPixelList[:] = []
       
-      #greenPixelList.sort()
-      #setGreen(finalPic, bluePixelList[listMidValue])
-      #greenPixelList[:] = []
+        greenPixelList.sort()
+        setGreen(finalPicPixel, greenPixelList[listMidValue])
+        greenPixelList[:] = []
       
-      #bluePixelList.sort()
-      #setBlue(finalPic, bluePixelList[listMidValue])
-      #bluePixelList[:] = []
-      return
+        bluePixelList.sort()
+        setBlue(finalPicPixel, bluePixelList[listMidValue])
+        bluePixelList[:] = []
      
---
+++ test.py
@@ -0,0 +1,6 @@
+def test():
+  sum = 0
+  for num in range (1,10):
+    for nums in range (1,3):
+      sum += 1
+  print sum
