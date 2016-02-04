1c5ee968503cfa1bc5704c75fd1bc8dda7bb4663
matt <mwall@csumb.edu>
2016-02-04 11:40:46 -0800

    added lighten, negate, and grayscale filters

--- FilterDriver2.py
+++ FilterDriver2.py
@@ -1,26 +1,77 @@
 def filterDriver2():
-  numPics = requestNumber("How Many Pictures?")
+  filter = requestString("Filter Type? medain, mean, negate, lighten, grayscale")  
   dateTime = requestString("What is the date and time? MM-dd-HHmm")
-  filter = requestString("Filter Type? Mean or Median?")
   filter.lower()
-  if filter == "median":
+  if filter == "median" or filter == "mean":
+    numPics = requestNumber("How Many Pictures?")
+    if filter == "median":
       medianFilter(numPics, dateTime)
-  elif filter == "mean":
+    else:
       meanFilter(numPics, dateTime)
+      
+  if filter == "negate" or filter == "lighten" or filter == "grayscale":
+    file = pickAFile()
+    picture = makePicture(file)
+  
+  if filter == "negate":
+    negate(picture, dateTime)
+    
+  if filter == "lighten":
+    lightenPixel(picture, dateTime)
+    
+  if filter == "grayscale":
+    grayScale(picture, dateTime)
+
+  
+def grayScale(picture, dateTime):
+  for pixel in getPixels(picture):
+    newRed = getRed(pixel)*0.299
+    newGreen = getGreen(pixel)*0.587
+    newBlue = getBlue(pixel)* 0.114
+    luminance = newRed+newGreen+newBlue
+    setColor(pixel, makeColor(luminance,luminance,luminance))
+  
+  writePictureTo(picture, "C:\cst205\Project1Images\Edited\GrayScaleEditPic" + dateTime + ".png")
+  repaint(picture)
+    
+def lightenPixel(picture, dateTime):
+  for pixel in getPixels(picture):
+    color=getColor(pixel)
+    color=makeLighter(color)
+    setColor(pixel, color)
+  
+  writePictureTo(picture, "C:\cst205\Project1Images\Edited\LightenEditPic" + dateTime + ".png")
+  repaint(picture)
+    
+def negate(picture, dateTime):
+  for px in getPixels(picture):
+    red=getRed(px)
+    green=getGreen(px)
+    blue=getBlue(px)
+    negColor=makeColor(255 - red, 255 - green, 255 - blue)
+    setColor(px, negColor)
+    
+  writePictureTo(picture, "C:\cst205\Project1Images\Edited\NegateEditPic" + dateTime + ".png")
+  repaint(picture)
       
 def medianFilter(numPics, dateTime):   
   redPixelList = []
   greenPixelList = []
   bluePixelList = []   
   pictureList = []  
-  listMidValue = 5
+  midValue = 0
+  
+  if numPics % 2 == 0:
+    midValue = int(numPics/2)
+  else:
+    midValue = int(numPics/2 + 1)
       
   for num in range (1, numPics+1):
     file = pickAFile()
     picture = makePicture(file)
     pictureList.append(picture)
   
-  finalPic = makePicture(pictureList[0])
+  finalPic = pictureList[0]
   finalPicPixel = getPixel(finalPic, 0,0)
   
   for pixelLength in range (0, getHeight(finalPic)):
@@ -34,18 +85,18 @@ def medianFilter(numPics, dateTime):
         bluePixelList.append(getBlue(pixel))
           
       redPixelList.sort()
-      setRed(finalPicPixel, redPixelList[listMidValue])
+      setRed(finalPicPixel, redPixelList[midValue])
       redPixelList[:] = []
       
       greenPixelList.sort()
-      setGreen(finalPicPixel, greenPixelList[listMidValue])
+      setGreen(finalPicPixel, greenPixelList[midValue])
       greenPixelList[:] = []
     
       bluePixelList.sort()
-      setBlue(finalPicPixel, bluePixelList[listMidValue])
+      setBlue(finalPicPixel, bluePixelList[midValue])
       bluePixelList[:] = []
         
-  writePictureTo(finalPic, 'C:\cst205\Project1Images\MedianEditPic" + dateTime + ".png')
+  writePictureTo(finalPic, "C:\cst205\Project1Images\Edited\MedianEditPic" + dateTime + ".png")
   repaint(finalPic)
     
 def meanFilter(numPics, dateTime):   
@@ -96,7 +147,7 @@ def meanFilter(numPics, dateTime):
       blueSum = 0
       bluePixelList[:] = []
   
-  fileWriteLocation = "C:\\cst205\\Project1Images\MeanEditPic" + dateTime + ".png"    
+  fileWriteLocation = "C:\\cst205\\Project1Images\Edited\MeanEditPic" + dateTime + ".png"    
   writePictureTo(finalPic, fileWriteLocation)
   repaint(finalPic)
 
--- FilterDriver3.py
+++ FilterDriver3.py
@@ -1,7 +1,112 @@
-class picData()
+class PicData(numPics, dateTime):
+  numPics
+  dateTime
+  def __init__(self):
+    self.numPics = numPics
+    self.dateTime = dateTime
 
-
-	redPixelList = []
-	greenPixelList = []
-	bluePixelList = []   
-	pictureList = []  
+    redPixelList = []
+    greenPixelList = []
+    bluePixelList = []  		
+	
+def filterDriver2():
+  numPics = requestNumber("How Many Pictures?")
+  dateTime = requestString("What is the date and time? MM-dd-HHmm")
+  picData = PicData(numPics, dateTime)
+  filter = requestString("Filter Type? Mean or Median?")
+  filter.lower()
+  if filter == "median":
+      medianFilter(picData)
+  elif filter == "mean":
+      meanFilter(picData)
+      
+def medianFilter(picData):   
+  pictureList = []  
+  listMidValue = 5
+      
+  for num in range (1, numPics+1):
+    file = pickAFile()
+    picture = makePicture(file)
+    pictureList.append(picture)
+  
+  finalPic = makePicture(pictureList[0])
+  finalPicPixel = getPixel(finalPic, 0,0)
+  
+  for pixelLength in range (0, getHeight(finalPic)):
+    for pixelWidth in range(0, getWidth(finalPic)):
+      for num in range (0, numPics):
+        picture = pictureList[num]        
+        pixel = getPixel(picture, pixelWidth, pixelLength)
+        finalPicPixel = getPixel(finalPic, pixelWidth, pixelLength)
+        picData.redPixelList.append(getRed(pixel))
+        picData.greenPixelList.append(getGreen(pixel))
+        picData.bluePixelList.append(getBlue(pixel))
+          
+      picData.redPixelList.sort()
+      setRed(finalPicPixel, picData.redPixelList[midValue(picData)])
+      redPixelList[:] = []
+      
+      picData.greenPixelList.sort()
+      setGreen(finalPicPixel, picData.greenPixelList[midValue(picData)])
+      greenPixelList[:] = []
+    
+      picData.bluePixelList.sort()
+      setBlue(finalPicPixel, picData.bluePixelList[midValue(picData)])
+      picData.bluePixelList[:] = []
+        
+  writePictureTo(finalPic, 'C:\cst205\Project1Images\MedianEditPic" + dateTime + ".png')
+  repaint(finalPic)
+  
+def midValue(picData):
+	if picData.numPics % 2 == 0:
+		return picData.numPics/2
+	return picData.numPics/2 + 1
+    
+def meanFilter(picData):   
+  pictureList = []  
+  
+  redSum = 0
+  greenSum = 0
+  blueSum = 0
+      
+  for num in range (1, numPics+1):
+    file = pickAFile()
+    picture = makePicture(file)
+    pictureList.append(picture)
+  
+  finalPic = pictureList[0]
+  finalPicPixel = getPixel(finalPic, 0,0)
+  
+  for pixelLength in range (0, getHeight(finalPic)):
+    for pixelWidth in range(0, getWidth(finalPic)):
+      for num in range (0, numPics):
+        picture = pictureList[num]        
+        pixel = getPixel(picture, pixelWidth, pixelLength)
+        finalPicPixel = getPixel(finalPic, pixelWidth, pixelLength)
+        redSum += getRed(pixel) 
+        picData.redPixelList.append(getRed(pixel))
+        
+        greenSum += getGreen(pixel)
+        picData.greenPixelList.append(getGreen(pixel))
+        
+        blueSum += getBlue(pixel)
+        picData.bluePixelList.append(getBlue(pixel))
+          
+      mean = redSum / numPics
+      setRed(finalPicPixel, mean)
+      redSum = 0
+      picData.redPixelList[:] = []
+      
+      mean = greenSum / numPics
+      setGreen(finalPicPixel, mean)
+      greenSum = 0
+      picData.greenPixelList[:] = []
+      
+      mean = blueSum / numPics
+      setBlue(finalPicPixel, mean)
+      blueSum = 0
+      picData.bluePixelList[:] = []
+  
+  fileWriteLocation = "C:\\cst205\\Project1Images\MeanEditPic" + dateTime + ".png"    
+  writePictureTo(finalPic, fileWriteLocation)
+  repaint(finalPic)
